c9e5d733e1d2e00e786fee0e9b6d66c6940ab5a8
Justin Sexton <justinsexton.dev@gmail.com>
2020-10-01 17:50:14 -0500

    Added pagination helper solution to project

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ Programming challenges written in the Python programming language.
 - Greed is Good (https://www.codewars.com/kata/5270d0d18625160ada0000e4)
 - Calculating with Functions (https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39)
 - Last Digit of Large Number (https://www.codewars.com/kata/5511b2f550906349a70004e1)
+- Pagination Helper (https://www.codewars.com/kata/515bb423de843ea99400000a)
 
 #### Kyu 6
 - Is a Number Prime (https://www.codewars.com/kata/5262119038c0985a5b00029f)
---
+++ tests/codewars/test_pagination.py
@@ -0,0 +1,39 @@
+import pytest
+
+from challenges.codewars.pagination import PaginationHelper
+
+
+class TestPaginationHelper:
+    @pytest.fixture
+    def helper(self):
+        return PaginationHelper(range(0, 11), 5)
+
+    def test_item_count_is_correctly_returned(self, helper):
+        assert 11 == helper.item_count()
+
+    def test_page_count_is_correctly_returned(self, helper):
+        assert 3 == helper.page_count()
+
+    @pytest.mark.parametrize("index,count", [
+        (0, 5),
+        (1, 5),
+        (2, 1),
+        (3, -1)
+    ])
+    def test_page_item_count_correctly_returns_how_many_items_are_on_a_page(self, helper, index, count):
+        assert count == helper.page_item_count(index)
+
+    @pytest.mark.parametrize("index,page", [
+        (10, 2),
+        (9, 1),
+        (5, 1),
+        (4, 0),
+        (2, 0),
+        (0, 0),
+        (-1, -1),
+        (-10, -1),
+        (11, -1),
+        (20, -1)
+    ])
+    def test_page_index_correctly_returns_which_page_an_index_belongs_too(self, helper, index, page):
+        assert page == helper.page_index(index)
