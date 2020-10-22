9017990daf4bfdc87023db24fccd722f012ce379
Justin Sexton <justinsexton.dev@gmail.com>
2020-10-21 22:15:58 -0500

    Added lonely character solution to project

---
+++ challenges/codewars/lonely.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from typing import List
+
+
+def loneliest(string: str) -> List[str]:
+    count = 0
+    space_dict = defaultdict(int)
+    last_character = None
+    for character in string.strip():
+        if character == ' ':
+            count += 1
+        else:
+            space_dict[character] = count
+
+            if last_character:
+                space_dict[last_character] = space_dict[last_character] + count
+
+            last_character = character
+            count = 0
+
+    highest = max(space_dict.values())
+    return [character for character, space_count in space_dict.items() if space_count == highest]
