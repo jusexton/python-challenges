997dcc2c297458d890024f3688a744708edc9c78
Justin Sexton <justinsexton.dev@gmail.com>
2020-12-09 15:00:04 -0600

    Added comment adding context to a line of code

--- challenges/leetcode/diagonalsum.py
+++ challenges/leetcode/diagonalsum.py
@@ -9,4 +9,6 @@ def diagonal_sum(matrix: List[List[int]]) -> int:
         summation += matrix[size - index - 1][index]
 
     even_length = size % 2 == 0
+    # If the matrix has an odd length the middle value is added twice
+    # Subtract the value in this scenario
     return summation if even_length else summation - matrix[size // 2][size // 2]
