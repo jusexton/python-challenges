903d6f90bb000a57b09a320cb3463470b38ccff0
Justin Sexton <justinsexton.dev@gmail.com>
2020-12-22 00:52:01 -0600

    Added character frequency solution

---
+++ challenges/codewars/charfreq.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+def character_freq(value: str) -> str:
+    value = value.replace(' ', '').lower()
+    counter = Counter(value)
+    return ','.join(f'{key}:{"*" * value}' for key, value in counter.items())
