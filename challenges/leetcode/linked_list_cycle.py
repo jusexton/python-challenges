8776200e2d0b492542a16e5e3968d9577625ddcb
jusexton <justinsexton.dev@gmail.com>
2024-12-30 13:43:10 -0600

    Added linked list cycle solution.

---
+++ challenges/leetcode/linked_list_cycle.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def has_cycle(self, head: ListNode | None) -> bool:
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        assert slow
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
